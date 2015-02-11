236fb36c41368c6986146c115f0819066ffe6685
THazelwood <Travbot5000@gmail.com>
2015-02-11 00:19:47 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+Template.blog.events({
+    'submit #blogForm': function(e) {
+        e.preventDefault();
+        var title = $('#blogTitle').val();
+        var body = $('#blogBody').val();
+        if (title.length && body.length) {
+            Meteor.call('submitPost', title, body);
+        }
+    }
+})
+
+Template.listBlogs.blogs = function() {
+    return Blogs.find();
+}
