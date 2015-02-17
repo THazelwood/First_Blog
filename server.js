abef8aae67ab24abd916ed7c69b364516add10cb
THazelwood <Travbot5000@gmail.com>
2015-02-17 09:49:22 -0600

    Update server.js
    added method to send Email still needs to be adjusted

--- server.js
+++ server.js
@@ -4,4 +4,11 @@ Meteor.methods({
 		console.log(body);
 		Blogs.insert({title:title, body:body});
 	}
+	
+	'sendEmail': function(to, from, subject, text){
+		//check([to,from,subject,text],[String]);
+		this.unblock();
+
+		Email.send({to: to, from: from, subject: title, text: body});
+	}
 })
